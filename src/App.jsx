30f3531e63b584ad305c4b550cf11cd1c72e6365
Shamem-Miah <shamemmiah2@gmail.com>
2024-12-03 21:03:01 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import Component from "./myComponent.jsx";
+import Button from "./button.jsx";
+function App() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
+
+export default App;
